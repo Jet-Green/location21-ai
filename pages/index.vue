b79e5be383400b1069c247c316f25cea9815f870
Jet-Green <grishadzyin@gmail.com>
2025-05-05 22:32:46 +0500

    init, add vuetify and icons

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container>
+    <v-row class="d-flex justify-center">
+      <v-col cols="12" md="10" xl="8">
+        <v-card variant="outlined">
+          главная
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<style></style>
