3c25e4e26328cedb9d5f5baa33c3c9802149c61c
Jet-Green <grishadzyin@gmail.com>
2025-05-08 23:05:25 +0500

    add ai page

---
+++ pages/ai.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+async function submit() {
+  try {
+    const response: any = await $fetch("https://functions.yandexcloud.net/d4eajvq0hfqcsdii8ge1", {
+      method: "POST",
+      body: {
+        userInput: "Привет"
+      }
+    })
+    if (response.status == 200) {
+      console.log(response);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+<template>
+  <v-container>
+    <v-row class="d-flex justify-center">
+      <v-col cols="12" md="10" xl="8">
+        <v-btn @click="submit">
+          сделать запрос в нейронку
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
