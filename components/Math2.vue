f73dae59ba5b532bb5da4c8a37623203c92de605
kstasiie <amkopylova@edu.hse.ru>
2025-05-18 22:50:44 +0500

    add Math2

--- components/Math2.vue
+++ components/Math2.vue
@@ -1,1 +1,18 @@
-<template></template>
+<script setup lang="ts">
+let number1 = ref<number>(13);
+let number2 = ref<number>(0);
+
+let result = computed(() => {
+  return Number(number1.value) + Number(number2.value);
+});
+</script>
+
+<template>
+  калькулятор
+  <v-text-field v-model="number1" type="number"></v-text-field>
+  <v-text-field v-model="number2" type="number"></v-text-field>
+
+  number1: {{ number1 }} <br />
+  number2: {{ number2 }} <br />
+  Результат: {{ result }}
+</template>
