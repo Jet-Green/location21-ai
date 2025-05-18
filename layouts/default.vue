5555c2c93ec2fc19f9794f687e0330f7aa1dc785
Jet-Green <grishadzyin@gmail.com>
2025-05-18 22:17:28 +0500

    Revert "start edu"
    This reverts commit e92b2b1be24b53ad1bf7bfed7a0b77dff57e61fe.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,10 @@
   </div>
 </template>
 <style scoped lang="scss">
-// .lamp-background {
-//   height: 100dvh;
-//   width: 100dvw;
-//   background-image: url('~/assets/images/lamp-background.jpg');
-//   background-size: cover;
-// }</style>
+.lamp-background {
+  height: 100dvh;
+  width: 100dvw;
+  background-image: url('~/assets/images/lamp-background.jpg');
+  background-size: cover;
+}
+</style>
